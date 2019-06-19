d8798ab031aad3b43aab20317131e1726fbe5baa
codejoncode <jonathanjholloway@gmail.com>
2019-06-18 23:45:47 -0500

    able to go to the page but when attempting to login I get an oops something went wrong  domain/login  not i the list of allowed callback URLs

--- index.js
+++ index.js
@@ -7,10 +7,62 @@ const bodyParser = require('body-parser');
 const session = require('express-session');
 const passport = require('passport');
 const Auth0Strategy = require('passport-auth0');
+var userInViews = require('./middleware/userInViews');
+var authRouter = require('./routes/auth');
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
 
-const routes = require('./routes/index')
+// ..
+
+
+
+//config express-session 
+const sess = {
+    secret: 'CHANGE THIS TO A RANDOM SECRET',
+    cookie: {}, 
+    resave: false, 
+    saveUninitialized: true 
+};
+
+const strategy = new Auth0Strategy(
+    {
+        domain: process.env.AUTH0_DOMAIN,
+        clientID: process.env.AUTH0_CLIENT_ID,
+        clientSecret: process.env.AUTH0_CLIENT_SECRET,
+        callbackURL:
+          process.env.AUTH0_CALLBACK_URL || 'http://localhost:4000/callback'
+    },
+
+    function (accessToken, refreshToken, extraParams, profile, done){
+        //accessToken is the token to call Auth0 API (not needed in most cases)
+        //extraParams.id_token has the JSON Web Token 
+        //profile has all the information from the user
+        return done(null, profile); 
+    }
+);
+
+
+passport.use(strategy);
+
+// You can use this section to keep a smaller payload
+passport.serializeUser(function (user, done) {
+    done(null, user);
+  });
+  
+  passport.deserializeUser(function (user, done) {
+    done(null, user);
+  });
+
 
 const app = express();
+
+if(app.get('env') === 'production'){
+    sess.cookie.secure = true;/// serve secure cookies, requires https
+}
+
+app.use(session(sess))
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.use(logger('dev'));
 app.use(bodyParser.json());
@@ -23,10 +75,14 @@ app.use(session({
 }));
 app.use(passport.initialize());
 app.use(passport.session());
+app.use(userInViews());
 
-app.use('/', routes); 
+
+app.use('/', authRouter);
+app.use('/', indexRouter);
+app.use('/', usersRouter);
 
 app.listen(4000, (err) => {
     if (err) console.log(err.message);
-    console.log("App running on port 3000 of localhost");
+    console.log("App running on port 4000 of localhost");
   });
