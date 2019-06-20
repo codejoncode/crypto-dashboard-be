b7df1c4930c67b3125bc84a91a38c07d47f54540
codejoncode <jonathanjholloway@gmail.com>
2019-06-19 23:10:44 -0500

    removed some files I no longer need after doing some research on some issues

--- index.js
+++ index.js
@@ -1,88 +1,9 @@
-
-const express = require('express');
-const path = require('path');
-const logger = require('morgan');
-const cookieParser = require('cookie-parser');
-const bodyParser = require('body-parser');
-const session = require('express-session');
-const passport = require('passport');
-const Auth0Strategy = require('passport-auth0');
-var userInViews = require('./middleware/userInViews');
-var authRouter = require('./routes/auth');
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-
-// ..
+require('dotenv').config();
+const server = require('./server.js');
+const PORT = process.env.PORT || 4001;
 
 
-
-//config express-session 
-const sess = {
-    secret: 'CHANGE THIS TO A RANDOM SECRET',
-    cookie: {}, 
-    resave: false, 
-    saveUninitialized: true 
-};
-// AUTH0_CALLBACK_URL=https://crypto-dashboard.auth0.com/callback/login
-const strategy = new Auth0Strategy(
-    {
-        domain: process.env.AUTH0_DOMAIN,
-        clientID: process.env.AUTH0_CLIENT_ID,
-        clientSecret: process.env.AUTH0_CLIENT_SECRET,
-        callbackURL:
-          process.env.AUTH0_CALLBACK_URL || 'http://localhost:3000/'
-    },
-
-    function (accessToken, refreshToken, extraParams, profile, done){
-        //accessToken is the token to call Auth0 API (not needed in most cases)
-        //extraParams.id_token has the JSON Web Token 
-        //profile has all the information from the user
-        return done(null, profile); 
-    }
-);
-
-
-passport.use(strategy);
-
-// You can use this section to keep a smaller payload
-passport.serializeUser(function (user, done) {
-    done(null, user);
-  });
-  
-  passport.deserializeUser(function (user, done) {
-    done(null, user);
-  });
-
-
-const app = express();
-
-if(app.get('env') === 'production'){
-    sess.cookie.secure = true;/// serve secure cookies, requires https
-}
-
-app.use(session(sess))
-app.use(passport.initialize());
-app.use(passport.session());
-
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: false}));
-app.use(cookieParser());
-app.use(session({
-  secret: 'shhhhhhhhh',
-  resave: true,
-  saveUninitialized: true
-}));
-app.use(passport.initialize());
-app.use(passport.session());
-app.use(userInViews());
-
-
-app.use('/', authRouter);
-app.use('/', indexRouter);
-app.use('/', usersRouter);
-
-app.listen(4000, (err) => {
+server.listen(PORT, (err) => {
     if (err) console.log(err.message);
-    console.log("App running on port 4000 of localhost");
+    console.log(`App running on port ${PORT} of localhost`);
   });
--- routes/index.js
+++ routes/index.js
@@ -1,45 +1,7 @@
-// require("dotenv").config();
-// const express = require("express");
-// const passport = require("passport");
-// const ensureLoggedIn = require("connect-ensure-login").ensureLoggedIn();
-// const request = require("request");
 
-// const env = {
-//   AUTH0_CLIENT_ID: process.env.AUTH0_CLIENT_ID,
-//   AUTH0_DOMAIN: process.env.AUTH0_DOMAIN,
-//   AUTH0_CALLBACK_URL: process.env.AUTH0_CALLBACK_URL
-// };
-
-// const router = express.Router();
-
-// router.get("/", (req, res) => {
-//   res.send("You are getting data");
-// });
-
-// router.get(
-//   "/login",
-//   passport.authenticate("auth0", {
-//     clientID: env.AUTH0_CLIENT_ID,
-//     domain: env.AUTH0_DOMAIN,
-//     redirectUri: env.AUTH0_CALLBACK_URL,
-//     responseType: "code",
-//     scope: "openid profile email"
-//   }),
-//   (req, res) => {
-//     res.redirect("/");
-//   }
-// );
-
-// router.get("/logout", (req, res) => {
-//   res.send("You are on the logout page");
-// });
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function (req, res, next) {
-  res.render('index', { title: 'Auth0 Webapp sample Nodejs' });
-});
 
 module.exports = router;
 
