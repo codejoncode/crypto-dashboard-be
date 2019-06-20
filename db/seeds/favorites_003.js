cfcbbd9f6952ccdbf833a2d712e0faec4b2c4650
codejoncode <jonathanjholloway@gmail.com>
2019-06-20 00:06:43 -0500

    added in a seed for the favorites also will delete this later likely though I am considering analyzing many users different favorites and showing this on the dashboard in which case I will add more if I go this route

--- db/seeds/favorites_003.js
+++ db/seeds/favorites_003.js
@@ -5,9 +5,7 @@ exports.seed = function(knex, Promise) {
     .then(function () {
       // Inserts seed entries
       return knex('table_name').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
+        {users_id : 3, favorites: "BTC ETH XMR DOGE"}
       ]);
     });
 };
