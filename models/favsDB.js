29f7e61fa7e982be8aabce85a3d0facb558ebb17
codejoncode <jonathanjholloway@gmail.com>
2019-06-20 19:27:52 -0500

    favsDB and favs rotue for updated created

--- models/favsDB.js
+++ models/favsDB.js
@@ -6,9 +6,24 @@ const getFavs  = () => {
     return db("favs").orderBy("id", "desc")
 }
 
+// get one fav  
+const getUserFav = (users_id) => {
+    return db("favs")
+      .where({users_id})
+}
+
+const updateUserFav = async (users_id, fav) => {
+    await db("favs")
+      .where({ users_id })
+      .update({ fav })
+    return getUserFav(users_id)
+}
+
 
 //export all of the functions 
 
 module.exports = {
     getFavs,
+    getUserFav,
+    updateUserFav,
 }
--- routes/favs.js
+++ routes/favs.js
@@ -1,13 +1,36 @@
 const express = require('express');
 const router = express.Router();
-const favsDB = require("../models/favsDB")
-
+const favsDB = require("../models/favsDB");
+const coinsDB = require("../models/coinsDB");
 
 router.get("/", (req, res) => {
     return favsDB
       .getFavs()
       .then(results => res.status(200).json(results))
       .catch(err => res.status(500).json({error: `Failed to get favs ${err}`}))
+})
+
+router.put("/:id", (req, res) => {
+  let moveForward = true; 
+  const { id } = req.params;
+  const { fav } = req.body; 
+  const favUpperCase = fav.toUpperCase(); 
+  coinsDB.getCoinByName(favUpperCase)
+  .then(count => {
+    if(Number(count[0].count) === 0){
+      moveForward = false; 
+      return res.status(409).json({ error: `There is no ${favUpperCase} data please remove or check the spelling of the coin symbol ${favUpperCase}. Thank you.`})
+    }
+  })
+  .catch(err => res.status(500).json(err))
+
+  if (moveForward === true){
+    return favsDB
+      .updateUserFav(id, favUpperCase)
+      .then(results => res.status(200).json(results))
+      .catch(err => res.status(500).json(err))
+  }
+    
 })
 
 module.exports = router;
